9e22e6db4a42225e785612bd45f826cf0afb17f6
diogosalesdev <diogosalesdev@gmail.com>
2021-03-16 16:46:04 -0300

    Forms and modal created

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import { createServer } from 'miragejs';
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
 
-createServer({
-  routes() {
-    this.namespace = 'api';
-
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'food',
-          createdAt: new Date()
-        }
-      ]
-    });
-  }
-});
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <GlobalStyle />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
     </>
   );
 }
